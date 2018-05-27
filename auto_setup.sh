6f32d4faca969d8933d66fc9ee5d92dfc73385a7
Yohan GREMBER <yohan.grember@gmail.com>
2018-05-27 21:06:37 +0200

    auto setup

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,3 +1,4 @@
+mv ~/vim-config ~/.vim
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && \
 git clone https://github.com/davidhalter/jedi-vim && \
@@ -7,4 +8,4 @@ git clone https://github.com/bps/vim-textobj-python && \
 git clone https://github.com/tpope/wellle/targets.vim && \
 git clone https://github.com/AndrewRadev/sideways.vim &&\
 git clone https://github.com/scrooloose/nerdcommenter
-
+ln -s ~/.vimrc ~/.vim/vimrc
