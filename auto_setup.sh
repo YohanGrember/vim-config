d1329ac13ed08296ac9e76f5a95dde01bf30f9cd
Yohan GREMBER <yohan.grember@gmail.com>
2018-05-27 21:27:56 +0200

    remove most mv from auto_setup

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,4 +1,4 @@
-mv ~/vim-config ~/.vim
+mv ~/.vimrc ~/.vimrc.old
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && \
 git clone https://github.com/davidhalter/jedi-vim && \
@@ -8,4 +8,4 @@ git clone https://github.com/bps/vim-textobj-python && \
 git clone https://github.com/tpope/wellle/targets.vim && \
 git clone https://github.com/AndrewRadev/sideways.vim &&\
 git clone https://github.com/scrooloose/nerdcommenter
-ln -s ~/.vimrc ~/.vim/vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
