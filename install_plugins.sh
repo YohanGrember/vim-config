8248688ae239599beed532af1500ba104c023b56
Yohan GREMBER <yohan.grember@gmail.com>
2018-05-27 20:59:20 +0200

    automate plugin installation

---
+++ install_plugins.sh
@@ -0,0 +1,10 @@
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle && \
+git clone https://github.com/davidhalter/jedi-vim && \
+git clone https://github.com/tpope/vim-surround && \
+git clone https://github.com/tpope/vim-fugitive && \
+git clone https://github.com/bps/vim-textobj-python && \
+git clone https://github.com/tpope/wellle/targets.vim && \
+git clone https://github.com/AndrewRadev/sideways.vim &&\
+git clone https://github.com/scrooloose/nerdcommenter
+
